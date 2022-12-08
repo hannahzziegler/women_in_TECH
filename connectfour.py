5fee4f2275c8f0b576093608dc6df18f5825f707
warriorcat69 <tasfiahaque816@gmail.com>
2022-12-07 20:58:15 -0500

    Update connectfour.py
    Changed from == False to if not

--- connectfour.py
+++ connectfour.py
@@ -164,7 +164,7 @@ class Board:
         state = self.state()
         while True:
             column = int(player.turn(state))
-            if column.isdigit() == False or (column < 1 or 7 < column):
+            if not column.isdigit() or (column < 1 or 7 < column):
                 # user did not give an integer OR user gave invalid integer
                 input("You must choose a number between 1 and 7 OR use your "
                       "power-up. Enter a new column number or type "
